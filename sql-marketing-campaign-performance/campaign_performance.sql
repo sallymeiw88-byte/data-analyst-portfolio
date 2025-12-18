83bcf9caacb2b4363a80cdf7461115059518b9cb
sallymei-data <sallymeiw88@gmail.com>
2025-12-18 11:56:09 -0500

    Update campaign_performance.sql

--- sql-marketing-campaign-performance/campaign_performance.sql
+++ sql-marketing-campaign-performance/campaign_performance.sql
@@ -1,1 +1,11 @@
+SELECT
+  ucm.campana_descripcion AS nombre_campana,
+  SUM(uvb.valor_booking)  AS ingresos,
+  COUNT(*)                AS viajes_completados
+FROM uber_viajes_bookings AS uvb
+LEFT JOIN uber_campanas_mercadeo AS ucm
+  ON uvb.campana_id = ucm.campana_ID
+WHERE uvb.estado_booking = 'Completed'
+GROUP BY ucm.campana_descripcion
+ORDER BY ingresos DESC;
 
